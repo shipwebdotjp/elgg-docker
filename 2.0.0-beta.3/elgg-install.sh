b2eccbe37e4400ae7184f0786cf123b8c610ab39
Kévio Castro <keviocastro@gmail.com>
2015-10-30 17:15:52 -0200

    environment variables

--- 2.0.0-beta.3/elgg-install.sh
+++ 2.0.0-beta.3/elgg-install.sh
@@ -2,20 +2,19 @@
 
 MYSQL_LOOPS="20"
 MYSQL_PORT="$MYSQL_PORT"
-MYSQL_HOST="$MYSQL_HOST"
 MYSQL_USER="$MYSQL_USER"
 MYSQL_PASS="$MYSQL_PASS"
-ELGG_DB_NAME="$ELGG_DB_NAME"
+ELGG_DB_HOST="$ELGG_DB_HOST"
 
 #wait for mysql
 i=0
-while ! netcat $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
+while ! netcat $ELGG_DB_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
   i=`expr $i + 1`
   if [ $i -ge $MYSQL_LOOPS ]; then
-    echo "$(date) - ${MYSQL_HOST}:${MYSQL_PORT} still not reachable, giving up."
+    echo "$(date) - ${ELGG_DB_HOST}:${MYSQL_PORT} still not reachable, giving up."
     exit 0
   fi
-  echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}... 1/$MYSQL_LOOPS."
+  echo "$(date) - waiting for ${ELGG_DB_HOST}:${MYSQL_PORT}... $i/$MYSQL_LOOPS."
   sleep 1
 done
 
