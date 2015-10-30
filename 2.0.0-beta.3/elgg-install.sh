d5260fda647d1b4a2a287f2bc3d605c2215c2f7d
Kévio Castro <keviocastro@gmail.com>
2015-10-30 16:14:13 -0200

    Não é mais necessário excluir a base de dados durante a instalação do elgg.

--- 2.0.0-beta.3/elgg-install.sh
+++ 2.0.0-beta.3/elgg-install.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-# Wait for database to get available
 
 MYSQL_LOOPS="20"
-MYSQL_HOST="$ELGG_DB_HOST"
+MYSQL_PORT="$MYSQL_PORT"
+MYSQL_HOST="$MYSQL_HOST"
 MYSQL_USER="$MYSQL_USER"
 MYSQL_PASS="$MYSQL_PASS"
-ELGG_DB_HOST="$ELGG_DB_HOST"
 ELGG_DB_NAME="$ELGG_DB_NAME"
-
-
 
 #wait for mysql
 i=0
@@ -21,13 +18,5 @@ while ! netcat $MYSQL_HOST $MYSQL_PORT >/dev/null 2>&1 < /dev/null; do
   echo "$(date) - waiting for ${MYSQL_HOST}:${MYSQL_PORT}... 1/$MYSQL_LOOPS."
   sleep 1
 done
-
-# Recreates the base Rlgg
-if ! [ $(mysql -N -s -u $MYSQL_USER -p $MYSQL_PASS -h $MYSQL_HOST -e \
-    "select count(*) from information_schema.tables where \
-        table_schema='$ELGG_DB_NAME' and table_name='elgg_entities';") -eq 1 ]; then
-    
-    mysql -u $MYSQL_USER -p $MYSQL_PASS -h $MYSQL_HOST -e "DROP DATABASE $ELGG_DB_NAME; CREATE DATABASE $ELGG_DB_NAME;"
-fi
 
 php	/elgg-docker/elgg-install.php
